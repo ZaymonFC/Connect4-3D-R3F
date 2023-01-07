349035b2b66491b64031cdb2c8429dd7bf44e51e
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-01-07 15:44:53 +1000

    Constraints on placement
    Must be within bounds and above another cell (or on base layer)

--- src/App.js
+++ src/App.js
@@ -108,6 +108,15 @@ const useCells = () => {
 
   const addCell = (move, player) =>
     setCells((cells) => {
+      // bail if outside bounds
+      if (move.x < 0 || move.x >= CELL_COUNT) return cells
+      if (move.y < 0 || move.y >= CELL_COUNT) return cells
+      if (move.z < 0 || move.z >= CELL_COUNT) return cells
+
+      // must be above another cell
+      if (move.y > 0 && !cells.some(({ cell }) => cell.equals(move.clone().add(new Vector3(0, -1, 0))))) return cells
+
+      // check if already in list
       if (cells.find(({ cell }) => cell.equals(move))) {
         return cells
       } else {
