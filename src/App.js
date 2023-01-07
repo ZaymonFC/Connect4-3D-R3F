2662423bd1b022cf01b8b2412de2c8e8f1744d84
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-01-07 14:54:46 +1000

    Enumerate all possible winning lines

--- src/App.js
+++ src/App.js
@@ -1,7 +1,122 @@
-import { Center, OrbitControls, OrthographicCamera } from "@react-three/drei"
+import { Center, Line, OrbitControls, OrthographicCamera, Select } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import React, { Suspense, useState } from "react"
 import { atom, useAtom, useAtomValue } from "jotai"
+import { withLatestFrom } from "rxjs"
+import { Vector3 } from "three"
+
+const w = 4
+const h = 4
+const d = 4
+
+const lines = []
+
+// Rectilinears
+for (let y = 0; y < d; y++) {
+  for (let x = 0; x < w; x++) {
+    const line = []
+    for (let z = 0; z < h; z++) {
+      line.push(new Vector3(x, y, z))
+    }
+
+    lines.push(line)
+  }
+
+  for (let z = 0; z < w; z++) {
+    const line = []
+    for (let x = 0; x < h; x++) {
+      line.push(new Vector3(x, y, z))
+    }
+
+    lines.push(line)
+  }
+}
+
+// Diagonals
+for (let y = 0; y < d; y++) {
+  const line = []
+  for (let x = 0; x < w; x++) {
+    line.push(new Vector3(x, y, x))
+  }
+
+  lines.push(line)
+}
+
+for (let y = 0; y < d; y++) {
+  const line = []
+  for (let z = 0; z < w; z++) {
+    line.push(new Vector3(z, y, w - z - 1))
+  }
+
+  lines.push(line)
+}
+
+// Columns
+
+for (let x = 0; x < w; x++) {
+  for (let z = 0; z < d; z++) {
+    const line = []
+    for (let y = 0; y < h; y++) {
+      line.push(new Vector3(x, y, z))
+    }
+
+    lines.push(line)
+  }
+}
+
+function rotateLineInFourDirections(line) {
+  const lines = []
+  lines.push(line)
+
+  const l2 = [...line].map((v) =>
+    v
+      .clone()
+      .applyAxisAngle(new Vector3(0, 1, 0), Math.PI / 2)
+      .add(new Vector3(0, 0, w - 1)),
+  )
+  lines.push(l2)
+
+  const l3 = [...line].map((v) =>
+    v
+      .clone()
+      .applyAxisAngle(new Vector3(0, 1, 0), Math.PI)
+      .add(new Vector3(w - 1, 0, w - 1)),
+  )
+  lines.push(l3)
+
+  const l4 = [...line].map((v) =>
+    v
+      .clone()
+      .applyAxisAngle(new Vector3(0, 1, 0), (3 * Math.PI) / 2)
+      .add(new Vector3(w - 1, 0, 0)),
+  )
+  lines.push(l4)
+
+  return lines
+}
+
+// Stairs
+for (let x = 0; x < w; x++) {
+  const line = []
+  for (let y = 0; y < h; y++) {
+    line.push(new Vector3(x, y, y))
+  }
+
+  rotateLineInFourDirections(line).forEach((l) => lines.push(l))
+}
+
+console.log(lines)
+
+const line = []
+for (let x = 0; x < w; x++) {
+  line.push(new Vector3(x, x, x))
+}
+
+rotateLineInFourDirections(line).forEach((l) => lines.push(l))
+
+function randomHexcode() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16)
+}
 
 const Base = React.forwardRef(({ position, onClick }, ref) => {
   const [hovered, setHovered] = useState(false)
@@ -128,6 +243,12 @@ function Room() {
             }}
           />
         ))}
+        <group position={[0, BASE_OFFSET, 0]}>
+          {lines.map((l) => {
+            const col = randomHexcode()
+            return <Line lineWidth={1} points={l.map((p) => p.multiplyScalar(CELL_SPACING))} color={col} key={col} />
+          })}
+        </group>
       </group>
     </>
   )
@@ -174,7 +295,7 @@ export default function App() {
           ref={ref}
           minPolarAngle={Math.PI / 3.5}
           maxPolarAngle={Math.PI / 3.5}
-          enableRotate={false}
+          enableRotate={true}
           enablePan={false}
           enableDamping={true}
           dampingFactor={0.6}
